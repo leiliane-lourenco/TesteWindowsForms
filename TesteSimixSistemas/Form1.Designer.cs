cd8d80176f75878031cc472412c01c416ac0a23d
Leiliane <leilianelourenzo@gmail.com>
2022-09-14 18:18:32 -0300

    Add project files.

---
+++ TesteSimixSistemas/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace TesteSimixSistemas
+{
+    partial class FormularioInicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(671, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button3.Location = new System.Drawing.Point(671, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 48);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultado.Location = new System.Drawing.Point(50, 38);
+            this.resultado.Multiline = true;
+            this.resultado.Name = "resultado";
+            this.resultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultado.Size = new System.Drawing.Size(570, 379);
+            this.resultado.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button2.Location = new System.Drawing.Point(671, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 48);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FormularioInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TesteSimixSistemas.Properties.Resources.imagem_numeros;
+            this.ClientSize = new System.Drawing.Size(887, 455);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "FormularioInicial";
+            this.Text = "BEM VINDO A SIMIX SISTEMAS";
+            this.TransparencyKey = System.Drawing.Color.Red;
+            this.Load += new System.EventHandler(this.FormularioInicial_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox resultado;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TesteSimixSistemas/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace TesteSimixSistemas
+{
+    public partial class FormularioInicial : Form
+    {
+        public FormularioInicial()
+        {
+            InitializeComponent();
+        }       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var text = resultado.Text;
+
+            List<int> listaResultado = new List<int>();
+
+            for (int i = 1; i < 201; i++)
+            {
+                listaResultado.Add(i);
+            }
+
+            foreach (var item in listaResultado)
+            {
+                if (item % 3 == 0 & item % 5 == 0)
+                    resultado.Text += " Z\r\n";
+
+                else if (item % 3 == 0)
+                    resultado.Text += " X\r\n"; 
+
+                else if (item % 5 == 0)
+                    resultado.Text += " Y\r\n";
+
+                else
+                    resultado.Text += $" {item}\r\n";
+
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            resultado.Clear();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void FormularioInicial_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
